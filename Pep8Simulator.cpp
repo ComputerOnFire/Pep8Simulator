b3c1224bd87c76c786153261ceed50d2d668f6c8
John <computeronfire@protonmail.com>
2021-01-21 18:17:14 -0600

    add finished project

--- Pep8Simulator.cpp
+++ Pep8Simulator.cpp
@@ -1,9 +1,206 @@
-// Pep8Simulator.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// pep8simulator.cpp
+// ComputerOnFire
+//
+// ends with zz
+// this made storing variables to memory easier, and allows you to copy pep8 instructions directly in
+// input.txt is the input file
+// instructions seperated by line
+//
+// example:
+// 500048
+// 500069
+// zz
+// Hi
 //
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+using namespace std;
 
+struct combo1 {
+    unsigned int right : 3;
+    unsigned int mid : 1;
+    unsigned int left : 4;
+};
+struct combo2 {
+    unsigned int right : 3;
+    unsigned int left : 5;
+};
+struct combo3 {
+    unsigned int right : 1;
+    unsigned int left : 7;
+};
+
+union uint24 {
+    
+    struct instruction {
+        union opFull {
+            struct operand {
+                unsigned int right : 8;
+                unsigned int left : 8;
+            } op;
+            unsigned int whole : 16;
+        } oper;
+        union specifier {
+            combo1 com1;
+            combo2 com2;
+            combo3 com3;
+            unsigned int whole : 8;
+        } speci;
+    } split;
+    unsigned int fullInstr : 24;
+
+ } instr;//global scope, thanks :)
+
+int lineCount(ifstream file) {
+    string s;
+    int ltotal = 0;
+    while (!file.eof()) {
+        getline(file, s);
+         ++ltotal;
+    }
+    return ltotal;
+}
+ 
+unsigned int rotateLeft(unsigned int x, unsigned int n) {
+    if (!n) return x;
+    return (x << n) | (x >> (24 - n));//rotates a 24 bit int to the left by n
+}
+unsigned int rotateRight(unsigned int x, unsigned int n) {
+    if (!n) return x;
+    return (x >> n) | (x << (24 - n));//rotates a 24 bit int to the right by n
+}
 int main()
 {
-    std::cout << "Hello Worl!\n";
+    struct pep8 {
+        uint24 memory[256] = {};
+        uint16_t registers[4] = { 0,0,0,0 };//A, X, PC, SP
+    }pep;
+    ifstream file("input.txt");
+    if (!file.is_open()) {
+    cout << "File not found!" << endl;
+}
+
+    else
+    {
+        string line;
+        while (getline(file, line)) {
+            istringstream iss(line);
+            //uint24 a;
+            instr.fullInstr = stoi(line, 0, 16);
+            pep.memory[pep.registers[2]].fullInstr = instr.fullInstr; //register 2 is the program counter
+            //a little hackey but it works
+            pep.memory[pep.registers[2]].split.oper.whole = instr.fullInstr & 0xff; //splits work for accessing operand and specifier
+            pep.memory[pep.registers[2]].split.speci.whole = instr.fullInstr >> 16; //but the whole number is 16 when done this way
+            cout << "Registers: ";
+            for (int i = 0; i < 4; ++i) {
+                cout << pep.registers[i] << " ";
+            }
+            switch (pep.memory[pep.registers[2]].split.speci.com3.left) {//interperates instructions
+
+            case 0:
+                //return 0;
+                break;
+            case 12://0001 100r
+                //bitwise invert r
+                pep.registers[instr.split.speci.com3.right] = ~pep.registers[instr.split.speci.com3.right];
+                break;
+            case 14://0001 110r
+                //shift left r
+                pep.registers[instr.split.speci.com3.right] = pep.registers[instr.split.speci.com3.right] << 1;
+                break;
+            case 15:
+                //shift right r
+                pep.registers[instr.split.speci.com3.right] = pep.registers[instr.split.speci.com3.right] >> 1;
+                break;
+            case 16:
+                //rotate left r
+                pep.registers[instr.split.speci.com3.right] = rotateLeft(instr.split.speci.com3.right, 1);
+                break;
+            case 17:
+                //rotate right r
+                pep.registers[instr.split.speci.com3.right] = rotateRight(instr.split.speci.com3.right, 1);
+                break;
+            }
+            //cout << pep.memory[pep.registers[2]].split.speci.com2.left << endl;
+            switch (pep.memory[pep.registers[2]].split.speci.com2.left) {
+            case 6:
+                //decimal input
+                unsigned int input;
+                cout << "Input Decimal: ";
+                cin >> input;
+                pep.memory[instr.split.oper.op.right].split.oper.whole = input & 0xFFFF; //force input to 8 bits
+                break;
+            case 7:
+                //decimal output
+                unsigned int output;
+                output = (pep.memory[pep.registers[2]].split.speci.com2.right == 1) ? pep.memory[instr.split.oper.op.right].split.oper.whole : instr.split.oper.whole;//conditional to differentiate direct and immediate addressing
+                cout << "Output: "  << output << endl;
+                cout << endl;
+                break;
+            case 9:
+                //character input
+                char chinput;
+                cout << "Input Char: ";
+                cin >> chinput;
+                pep.memory[instr.split.oper.op.right].split.oper.whole = input & 0xFFFF;
+                break;
+            case 10:
+                //character output
+                char choutput;
+                choutput = (pep.memory[pep.registers[2]].split.speci.com2.right == 1) ? pep.memory[instr.split.oper.op.right].split.oper.whole : instr.split.oper.whole;
+                cout << "Output: " << choutput << endl;
+                cout << endl;
+                break;
+            }
+            //cout << pep.memory[pep.registers[2]].split.speci.com1.left << endl; test thingy
+            switch (pep.memory[pep.registers[2]].split.speci.com1.left) {
+            case 7:
+                //add to r
+                cout << pep.registers[instr.split.speci.com1.mid];
+                cout << ((pep.memory[pep.registers[2]].split.speci.com2.right == 1) ? pep.memory[instr.split.oper.op.right].split.oper.whole : instr.split.oper.whole);
+                pep.registers[instr.split.speci.com1.mid] = pep.registers[instr.split.speci.com1.mid] + ((pep.memory[pep.registers[2]].split.speci.com2.right == 1) ? pep.memory[instr.split.oper.op.right].split.oper.whole : instr.split.oper.whole);
+                break;
+            case 8:
+                //subtract from r
+                pep.registers[instr.split.speci.com1.mid] -= (pep.memory[pep.registers[2]].split.speci.com2.right == 1) ? pep.memory[instr.split.oper.op.right].split.oper.whole : instr.split.oper.whole;
+                break;
+            case 9:
+                //bitwise AND to r
+                pep.registers[instr.split.speci.com1.mid] = pep.registers[instr.split.speci.com1.mid] & ((instr.split.speci.com1.right) ? pep.memory[instr.split.oper.op.right].split.oper.whole : instr.split.oper.whole);
+                break;
+            case 10:
+                //bitwise OR to r
+                pep.registers[instr.split.speci.com1.mid] = pep.registers[instr.split.speci.com1.mid] | ((instr.split.speci.com1.right) ? pep.memory[instr.split.oper.op.right].split.oper.whole : instr.split.oper.whole) ;
+                break;
+            case 12:
+                //load r from memory
+                pep.registers[instr.split.speci.com1.mid] = (pep.memory[pep.registers[2]].split.speci.com2.right == 1) ? pep.memory[instr.split.oper.op.right].split.oper.whole : instr.split.oper.whole;
+                break;
+            case 13:
+                //load byte r from memory
+                pep.registers[instr.split.speci.com1.mid] = (pep.memory[pep.registers[2]].split.speci.com2.right == 1) ? pep.memory[instr.split.oper.op.right].split.oper.whole : instr.split.oper.whole ;
+                break;
+            case 14:
+                //store r to memory
+                pep.memory[instr.split.oper.op.right].split.oper.whole = pep.registers[instr.split.speci.com1.mid];
+                break;
+            case 15:
+                //store byte r to memory
+                pep.memory[instr.split.oper.op.right].split.oper.whole = pep.registers[instr.split.speci.com1.mid];
+                break;
+            }
+            cout << endl;
+            pep.registers[2]++;
+            //pep.registers[3]++;//3 byte per line, inputs stored in left operand
+            if (line.find("zz") != string::npos) {
+                return 0;//end program at zz (had trouble with 0000 0000 stop and storing inputs)
+            }
+           
+        }
+        file.close();
+    }
+    return 0;
 }
--- README.md
+++ README.md
@@ -1,42 +1,10 @@
-# Pep8Simulator
+* 8 bit ASM simulator, based off of the Pep 8 machine
 
-#PEP8 SIMULATOR PROJECT
+input.txt is the input file
+instructions seperated by line
 
-GOAL: Write a PEP/8 machine language simulator that simulates the PEP/8 computer and instruction set for only the following instructions:
-
-Instruction Specifier 	Instruction 	Addressing Modes 	Status Bits
-0000 0000 	Stop execution 	NA	
-0001 100r 	Bitwise invert r 	NA	NZ
-0001 110r 	Arithmetic shift left r 	NA	NZVC
-0001 111r 	Arithmetic shift right r 	NA	NZC
-0010 000r 	Rotate left r 	NA	C
-0010 001r 	Rotate right r 	NA	C
-0011 0aaa 	Decimal input trap 	d	NZV
-0011 1aaa 	Decimal output trap 	d, i	
-0100 1aaa 	Character input 	d	
-0101 0aaa 	Character output 	d, i	
-0111 raaa 	Add to r 	d, i	NZVC
-1000 raaa 	Subtract from r 	d, i	NZVC
-1001 raaa 	Bitwise AND to r 	d, i	NZ
-1010 raaa 	Bitwise OR to r 	d, i	NZ
-1100 raaa 	Load r from memory 	d, i	NZ
-1101 raaa 	Load byte from memory 	d, i	NZ
-1110 raaa 	Store r to memory 	d	
-1111 raaa 	Store byte r to memory 	d	
-
-
-
-
-
-
-Here are some specifics.
-
-1.	Use an array to represent the memory.
-2.	Use variables or arrays for the PEP8 registers. I recommend putting it all into a structure. I also recommend using an array of 16 bit values for A, X, PC, SP so that you can use the r bit from the instructions to point directly to A or X. You do NOT have to include the Status bits.
-3.	Use unions of structures to break up the registers and instructions into the correct bits (for example, use a structure that can be unioned to break up the 8 bit specifier into the following bit combos (4, 1, 3), (5, 3), (7, 1), (8). This will allow you to instantly extract the instruction, register and addressing modes from each instruction. Also use a union to break up the 16 bit operand into two 8 bit values.
-4.	Have the instructions be inputted either through the command line or better yet, through a file, in the following format for each instruction:
-a.	6 Hexadecimal characters per instruction
-5.	After each instruction in the program is executed, output the PEP8 machine register values.
-
-
-
+example:
+500048
+500069
+// zz
+// Hi
